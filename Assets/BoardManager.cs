01115777a7a91a1b2a272f2592776f29b6d303c5
sandsocks <max.sandejas@gmail.com>
2025-08-09 17:28:30 +0800

    yes

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -19,7 +19,7 @@ public class BoardManager : MonoBehaviour
     public List<GameObject> spawnedTiles = new List<GameObject>();
     long dirtBB = 0;                    //tree
     long desertBB = 0;                  //camel
-    long grainBB = 0;                   //tree
+    long grainBB = 0;                   //
     long pastureBB = 0;                 //flower
     long waterBB = 0;                   //fish
     long woodsBB = 0;                   //bug
@@ -31,13 +31,8 @@ public class BoardManager : MonoBehaviour
     long flowerBB = 0;                  //pasture
     long fishBB = 0;                    //water
     long bugBB = 0;                     //woods
+    long houseBB = 0;                   //grain
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     public void CreateBoard()
     {
         for (int r = 0; r < 8; r++)
@@ -52,10 +47,46 @@ public class BoardManager : MonoBehaviour
                     dirtBB = SetCellState(dirtBB, r, c);
                     PrintBB("Dirt", dirtBB);
                 }
+                else if (tile.tag == "Desert")
+                {
+                    desertBB = SetCellState(desertBB, r, c);
+                    PrintBB("Desert", desertBB);
+                }
+                else if (tile.tag == "Water")
+                {
+                    waterBB = SetCellState(waterBB, r, c);
+                    PrintBB("Water", waterBB);
+                }
+                else if (tile.tag == "Pasture")
+                {
+                    pastureBB = SetCellState(pastureBB, r, c);
+                    PrintBB("Pasture", pastureBB);
+                }
+                else if (tile.tag == "Woods")
+                {
+                    woodsBB = SetCellState(woodsBB, r, c);
+                    PrintBB("Woods", woodsBB);
+                }
+                else if (tile.tag == "Rock")
+                {
+                    rockBB = SetCellState(rockBB, r, c);
+                    PrintBB("Rock", rockBB);
+                }
+                else if (tile.tag == "Grain")
+                {
+                    grainBB = SetCellState(grainBB, r, c);
+                    PrintBB("Grain", grainBB);
+                }
+
                 spawnedTiles.Add(tile);
             }
         Debug.Log("Dirt Cells = ");         //print how many dirt we have
         InvokeRepeating("PlantTree", 0.25f, 0.25f);
+        InvokeRepeating("PlantCamel", 0.25f, 0.25f);
+        InvokeRepeating("PlantFish", 0.25f, 0.25f);
+        InvokeRepeating("PlantFlower", 0.25f, 0.25f);
+        InvokeRepeating("PlantBug", 0.25f, 0.25f);
+        InvokeRepeating("PlantSword", 0.25f, 0.25f);
     }
 
     void PrintBB(string name, long BB)
@@ -107,6 +138,84 @@ public class BoardManager : MonoBehaviour
             tree.transform.parent = spawnedTiles[rr * 8 + rc].transform;
             tree.transform.localPosition = Vector3.zero;
             treeBB = SetCellState(treeBB, rr, rc);
+        }
+    }
+
+    void PlantCamel()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(desertBB, rr, rc))
+        {
+            GameObject camel = Instantiate(camelPrefab);
+            camel.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            camel.transform.localPosition = Vector3.zero;
+            camelBB = SetCellState(treeBB, rr, rc);
+        }
+    }
+
+    void PlantSword()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(rockBB, rr, rc))
+        {
+            GameObject sword = Instantiate(swordPrefab);
+            sword.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            sword.transform.localPosition = Vector3.zero;
+            swordBB = SetCellState(swordBB, rr, rc);
+        }
+    }
+
+    void PlantFlower()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(pastureBB, rr, rc))
+        {
+            GameObject flower = Instantiate(flowerPrefab);
+            flower.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            flower.transform.localPosition = Vector3.zero;
+            flowerBB = SetCellState(flowerBB, rr, rc);
+        }
+    }
+
+    void PlantFish()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(waterBB, rr, rc))
+        {
+            GameObject fish = Instantiate(fishPrefab);
+            fish.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            fish.transform.localPosition = Vector3.zero;
+            fishBB = SetCellState(fishBB, rr, rc);
+        }
+    }
+
+    void PlantBug()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(woodsBB, rr, rc))
+        {
+            GameObject bug = Instantiate(bugPrefab);
+            bug.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            bug.transform.localPosition = Vector3.zero;
+            bugBB = SetCellState(bugBB, rr, rc);
+        }
+    }
+    
+    void PlantHouse()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);            //random row
+        int rc = UnityEngine.Random.Range(0, 8);            //random row
+        if (GetCellState(grainBB, rr, rc))
+        {
+            GameObject house = Instantiate(housePrefab);
+            house.transform.parent = spawnedTiles[rr * 8 + rc].transform;
+            house.transform.localPosition = Vector3.zero;
+            houseBB = SetCellState(houseBB, rr, rc);
         }
     }
 }
